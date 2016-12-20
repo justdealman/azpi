a6a21ad2d08262c075d65714cacea2f5e260d25b
justdealman <justdealman@gmail.com>
2016-12-20 16:50:05 +0700

    more functionality

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,33 @@ $(function() {
 			'background-size': 'cover'
 		});
 	});
+	$('.introduction-i').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		arrows: false,
+		dots: true,
+		draggable: true,
+		autoplay: true,
+  	autoplaySpeed: 5000,
+	});
+	$('.statistics-i ul li').each(function() {
+		var t = $(this);
+		t.find('div').circleProgress({
+				value: eval(t.find('div').attr('data')),
+				size: 147,
+				lineCap: 'round',
+				startAngle: -Math.PI/2,
+				thickness: 6,
+				emptyFill: 'transparent',
+				fill: { color: '#f5a623' },
+				animation: { duration: 1500 }
+		});
+		t.find('.count').counterUp({
+			delay: 10,
+			time: 1000
+		});
+	});
 	$('.map-i .lc .core').jScrollPane({
 		verticalDragMinHeight: 49,
     verticalDragMaxHeight: 49
@@ -25,7 +52,24 @@ $(function() {
 	$('.content-b .lc .nav > li > ul').siblings('a').on('click', function(e) {
 		e.preventDefault();
 		$(this).parent().toggleClass('active');
-	})
+	});
+	$('.fancybox-media').fancybox({
+		padding: 0,
+		helpers: {
+			media: {},
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	$('.fancybox-photo').fancybox({
+		padding: 0,
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
 	$('input, textarea').each(function() {
 		$(this).data('holder', $(this).attr('placeholder'));
 		$(this).focusin(function() {

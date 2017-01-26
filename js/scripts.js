3312b838f397979caec6779e42b6817e6f57907e
justdealman <justdealman@gmail.com>
2017-01-26 08:47:37 +0700

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
 ﻿$(function() {
-	var size = $('.news-i .core ul li').size();
-	var width = $('.news-i .core ul li').outerWidth();
-	$('.news-i .drag').slider({
-		min: 0,
-		max: eval($('.news-i .core ul li').size()-3),
-		slide: function (event,ui) {
-			$('.news-i .core ul').stop().animate({
-				'margin-left': -ui.value*width+'px'
-			}, 500);
-		}
-	});
-});
-$(function() {
+	function newsSlider() {
+		var enabled = false;
+		$(window).on('resize', function() {
+			var size = $('.news-i .core ul li').size();
+			var width = $('.news-i .core ul li').outerWidth();
+			var quantity = $('.news-i .core ul li').size();
+			if  ( Modernizr.mq('(max-width:1000px)') && Modernizr.mq('(min-width:701px)') ) {
+				var max = quantity-2;
+			} else if ( Modernizr.mq('(max-width:700px)') ) {
+				var max = quantity-1;
+			} else {
+				var max = quantity-3;
+			}
+			if ( enabled ) {
+				$('.news-i .drag').slider('destroy');
+				$('.news-i .core ul').css({
+					'margin-left': 0
+				})
+			}
+			$('.news-i .drag').slider({
+				min: 0,
+				max: max,
+				slide: function(event,ui) {
+					$('.news-i .core ul').stop().animate({
+						'margin-left': -ui.value*width+'px'
+					}, 500);
+				},
+				change: function(event,ui) {
+					$('.news-i .core ul').stop().animate({
+						'margin-left': -ui.value*width+'px'
+					}, 500);
+					console.log('asd');
+				}
+			});
+			enabled = true;
+		});
+		$('.news-i .core').on('swipeleft', function() {
+			var t = $('.news-i .drag');
+			t.slider('value',t.slider('value')+1);
+		});
+		$('.news-i .core').on('swiperight', function() {
+			var t = $('.news-i .drag');
+			t.slider('value',t.slider('value')-1);
+		});
+	}
+	newsSlider();
 	$('.img-bg').each(function() {
 		$(this).parent().css({
 			'background': 'url("'+$(this).attr('src')+'") no-repeat center center',
@@ -79,4 +112,71 @@ $(function() {
 			$(this).attr('placeholder', $(this).data('holder'));
 		});
 	});
+	var isMobile = false;
+	var justSwitched = false;
+	function detectDevice() {
+		var temp = isMobile;
+		if ( Modernizr.mq('(max-width:1000px)') ) {
+			isMobile = true;
+		} else {
+			isMobile = false;
+		}
+		if ( temp == isMobile ) {
+			justSwitched = false;
+		} else {
+			justSwitched = true;
+		}
+	}
+	function showMobileMenu() {
+		$('body').addClass('is-locked');
+		$('.mobile-drop').addClass('is-active');
+		$('.fade-bg').stop().fadeIn(300);
+	}
+	function hideMobileMenu() {
+		$('body').removeClass('is-locked');
+		$('.mobile-drop').removeClass('is-active');
+		$('.fade-bg').stop().fadeOut(300);
+	}
+	$(window).on('load resize', function() {
+		detectDevice();
+		if ( justSwitched ) {
+			if ( isMobile ) {
+				$('.video-i').detach().insertAfter('.news-i');
+				$('header > div').append('<span class="menu-mobile"></span>');
+				$('body').append('<span class="mobile-drag"></span>');
+				$('body').append('<div class="mobile-drop"></div>');
+				$('header .logo, nav').clone().appendTo($('.mobile-drop'));
+				$('.panel .nav').clone().appendTo($('.mobile-drop'));
+				$('.mobile-drop nav ul ul').each(function() {
+					$(this).siblings().addClass('has-sub');
+				});
+				$('.mobile-drop a.has-sub').on('click', function(e) {
+					e.preventDefault();
+					$(this).toggleClass('is-active');
+				});
+				$('.menu-mobile').on('click', function(e) {
+					e.preventDefault();
+					showMobileMenu();
+				});
+				$('.fade-bg').on('click', function(e) {
+					e.preventDefault();
+					hideMobileMenu();
+				});
+				$('.mobile-drop').on('swipeleft', function(e) {
+					e.preventDefault();
+					hideMobileMenu();
+				});
+				$('.mobile-drag').on('swiperight', function(e) {
+					e.preventDefault();
+					showMobileMenu();
+				});
+				$('.map-i .lc').detach().insertAfter('.map-i .rc');
+			} else {
+				$('.video-i').detach().insertAfter('.statistics-i');
+				$('.mobile-drop, .menu-mobile').remove();
+				$('.map-i .lc').detach().insertBefore('.map-i .rc');
+			}
+		}
+	});
+	$(window).trigger('resize');
 });
